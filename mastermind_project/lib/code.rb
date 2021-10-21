00463698e386d2000861714d20c15795a53783f8
Ramsey Njire <ramseynjire@gmail.com>
2021-10-21 18:08:50 +0300

    write initialize method

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -10,4 +10,9 @@ class Code
     array.all?{ |peg| POSSIBLE_PEGS.keys.include?(peg.upcase)} ? true : false
   end
 
+
+  def initialize(peg_array)
+    raise "One or more of the characters is not a valid peg" unless Code.valid_pegs?(peg_array)
+    @pegs = peg_array.map!{ |peg| peg.upcase }
+  end
 end
